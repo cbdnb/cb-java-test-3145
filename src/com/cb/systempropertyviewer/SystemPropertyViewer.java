5f811e39c4dbb09a72d618a5221b785b48eee7a4
Christian Baumann <k.c.baumann@t-online.de>
2024-07-30 23:26:50 +0200

    cvb

--- src/com/cb/systempropertyviewer/SystemPropertyViewer.java
+++ src/com/cb/systempropertyviewer/SystemPropertyViewer.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 public class SystemPropertyViewer {
 
 	public static void main(String[] args) {
-		System.out.println("Die Properties anzeigen, weil es wirklich nett ist:");
+		System.out.println("Die Properties anzeigen, weil es wirklich so nett ist:");
 		prontProp();
 		
 	}
